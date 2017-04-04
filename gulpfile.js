b56032a717dd43a695b090e40ac1c5552ca7511b
alnahas <alnahas1@hotmail.com>
2017-04-04 21:37:06 +0200

    add postcss , autoprefixer , simple vars , nested

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch')
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars =require('postcss-simple-vars'),
+nested = require('postcss-nested')
 
 gulp.task('default',function(){
     console.log('hello world')
@@ -10,7 +14,9 @@ gulp.task('html',function(){
 });
 
 gulp.task('style',function(){
-    console.log('imagine sass')
+   return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars,nested,autoprefixer]))
+    .pipe(gulp.dest('./app/assets/temp/styles'))
 })
 
 gulp.task('watch',function(){
